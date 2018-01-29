3a9256fe34625843e147d4f5d3ce0e945f7a65f6
babyisun@qq.com <babyisun@qq.com>
2018-01-29 11:24:05 +0800

    timestamp supported

--- script/itbaby.js
+++ script/itbaby.js
@@ -7,7 +7,7 @@
     //Demo
     var Demo = {
         date: function () {
-            $("#demo-date").html($.date() + "<br>" + $.date("2014-1-1"));
+            $("#demo-date").html($.date() + "<br>" + $.date("2014-1-1") + "<br>" + $.date(1514736000000));
             $("#demo-date-format").html($.date().format("yyyy-MM-dd hh:mm:ss") + "<br>" + $.date("2014-1-1").format("yyyy-MM-dd"));
             $("#demo-getAge").html($.date("2001-1-4").getAge());
             $("#demo-diffDate").html($.diffDate($.date(), $.date("2013-1-1")) + "<br>" + $.diffDate("2013-1-2", "2013-1-3", "hour"));
